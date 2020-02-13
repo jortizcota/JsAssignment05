fa817a4dc86c81628490732077ff2df7b6e71864
Ortiz Cota <josephortizcota@my.whccd.edu>
2020-02-12 20:08:41 -0800

    answered my first question of my assignment 5.

--- script.js
+++ script.js
@@ -3,10 +3,32 @@
 // Example output:
 // console.log(range(1, 10)); // outputs [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+function range(start, end) {
+  let array = [];
+
+  for (let i = start; i <= end; i++) {
+    array.push(i);
+  }
+
+  return array;
+}
+console.log(range(1, 10));
+console.log(range(4, 14));
+
 // 2. Next, write a sum function that takes an array of numbers and returns the sum of
 // these numbers. Run the example program and see whether it does indeed return 55.
 // Example output:
 // console.log(sum(range(1, 10))); // returns 55
+
+function sum(array) {
+  let total = 0;
+  for (let value of array) {
+    total += value;
+  }
+  return total;
+}
+console.log(sum(range(1, 10)));
+console.log(sum(range(4, 14)));
 
 // 3. Arrays have a reverse method that changes the array by inverting the order in which
 // its elements appear. For this exercise, write two functions, reverseArray and
